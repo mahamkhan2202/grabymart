544d17d14a0f3e5fcbf3357cd76da85d387d291b
mahamkhan2202 <mahamkhan2202@gmail.com>
2022-11-12 03:06:54 +0500

    6

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -7,12 +7,14 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
+import { Inputmodule } from 'src/app/components/input/input.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    Inputmodule,
     LoginPageRoutingModule
   ],
   declarations: [LoginPage]
